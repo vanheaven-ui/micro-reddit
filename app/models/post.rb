725072ae134c6190e5dcac7f78eaeedf976afe28
vanheaven <vanheaven6@gmail.com>
2020-09-04 13:11:52 +0300

    Create Comment data model
    Add data validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
 
   validates :title, :body,
             presence: true,
-            length: { minimum: 5 }
+            length: { minimum: 5 },
+            uniqueness: true
   validates :title, length: { maximum: 100 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   validates :email, :password, :username,
             presence: true,
-            length: { minimum: 5 }
+            length: { minimum: 5 },
+            uniqueness: true
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
   validates :password, length: { maximum: 20 }
 
   has_many :posts
+  has_many :comments
 end
