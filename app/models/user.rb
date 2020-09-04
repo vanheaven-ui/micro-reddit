4c6bb8e4ecd7745ac90774d25a8d6242cd82d2e2
vanheaven <vanheaven6@gmail.com>
2020-09-04 12:40:25 +0300

    Resolve rubocop fails in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :email, :password, :username,
-              presence: true,
-              length: { minimum: 5 }
+            presence: true,
+            length: { minimum: 5 }
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
   validates :password, length: { maximum: 20 }
 end
