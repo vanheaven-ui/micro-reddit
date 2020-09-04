b869a03b6054e39f9e9e992179c53ecdececf8e7
vanheaven <vanheaven6@gmail.com>
2020-09-04 12:55:48 +0300

    Create Post data model
    generate Post model
    
    Add validations to model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :title, :body,
+            presence: true,
+            length: { minimum: 5 }
+  validates :title, length: { maximum: 100 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
             length: { minimum: 5 }
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
   validates :password, length: { maximum: 20 }
+
+  has_many :posts
 end
